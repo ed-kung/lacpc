8061945999dc4427a8a02527ec8780fd1b30712b
ed-kung <edward.kung@gmail.com>
2026-07-09 00:46:12 -0700

    update tables and results

--- src/R/15-ordered-logit.R
+++ src/R/15-ordered-logit.R
@@ -125,107 +125,97 @@ coefs_df <- rbind(
 
 out_filename <- paste0(DATA_PATH, "/intermediate_data/cpc/ologit_regression_coefs.parquet")
 write_parquet(coefs_df, out_filename)
-# 
-# 
-# # ---- Oster (2019) robustness check
-# 
-# short_reg <- lm(
-#   build_fmla("outcome_y", vars1), 
-#   data = df
-# )
-# controlled_reg <- lm(
-#   build_fmla("outcome_y", c(vars1, vars2, cluster_fe, cd_fe, sfx_fe)),
-#   data = df
-# )
-# Rmax <- min(1.3*summary(controlled_reg)$r.squared, 1)
-# 
-# 
-# oster_delta <- o_delta(
-#   y = "outcome_y", 
-#   x = "atypicality",
-#   con = paste0(c(vars2, cluster_fe, cd_fe, sfx_fe), collapse=" + "),
-#   beta = 0,
-#   R2max = Rmax,
-#   type = "lm",
-#   data = df
-# )
-# 
-# out_filename <- paste0(DATA_PATH, "/intermediate_data/cpc/ologit_oster_delta.parquet")
-# write_parquet(oster_delta, out_filename)
-# 
-# 
-# # ---- Marginals
-# 
-# #m1 <- avg_slopes(r1)
-# #m1$regression_name <- "r1"
-# #m2 <- avg_slopes(r2)
-# #m2$regression_name <- "r2"
-# #m3 <- avg_slopes(r3)
-# #m3$regression_name <- "r3"
-# #m4 <- avg_slopes(r4)
-# #m4$regression_name <- "r4"
-# 
-# #marginals_df <- rbind(m1, m2, m3, m4)
-# 
-# #out_filename <- paste0(DATA_PATH, "/intermediate_data/cpc/ologit_regression_marginals.parquet")
-# #write_parquet(marginals_df, out_filename)
-# 
-# 
-# 
-# # ---- Regressions on non-delay outcomes
-# 
-# dfb <- filter(df, project_result!="DELAYED")
-# 
-# dfb$outcome <- dfb$outcome_y
-# dfb$outcome[dfb$outcome==1] <- 0
-# dfb$outcome[dfb$outcome==2] <- 1
-# 
-# dfb$log2_letters <- log2(1+dfb$n_support+dfb$n_oppose)
-# 
-# vars3 <- c("agenda_order", "num_agenda_items", "is_consent_calendar", 
-#            "log2_letters")
-# 
-# #sfx_fe_2 <- sfx_fe[sapply(df_nodelay[sfx_fe], var, na.rm=TRUE) != 0]
-# 
-# r1b <- glm(
-#   build_fmla("outcome", vars1),
-#   data=dfb, family=binomial(link="logit")
-# )
-# r2b <- glm(
-#   build_fmla("outcome", c(vars1,vars2,cluster_fe)),
-#   data=dfb, family=binomial(link="logit")
-# )
-# r3b <- glm(
-#   build_fmla("outcome", c(vars1,vars2,cluster_fe,cd_fe)),
-#   data=dfb, family=binomial(link="logit")
-# )
-# r4b <- glm(
-#   build_fmla("outcome", c(vars1,vars2,cluster_fe,cd_fe,sfx_fe)),
-#   data=dfb, family=binomial(link="logit")
-# )
-# r5b <- glm(
-#   build_fmla("outcome", c(vars1,vars3,cluster_fe,cd_fe,sfx_fe)),
-#   data=dfb, family=binomial(link="logit")
-# )
-# 
-# stargazer(
-#   r1b, r2b, r3b, r4b, r5b, type="text",
-#   keep=c(vars1, vars2, vars3),
-#   add.lines=list(
-#     c("Cluster FE",          "N", "Y", "Y", "Y", "Y"),
-#     c("Council District FE", "N", "N", "Y", "Y", "Y"),
-#     c("Case Suffix FE",      "N", "N", "N", "Y", "Y")
-#   )
-# )
-# 
-# 
-# coefs_df <- rbind(
-#   extract_reg(r1b, "r1b"),
-#   extract_reg(r2b, "r2b"),
-#   extract_reg(r3b, "r3b"),
-#   extract_reg(r4b, "r4b"),
-#   extract_reg(r5b, "r5b")
-# )
-# 
-# out_filename <- paste0(DATA_PATH, "/intermediate_data/cpc/ologit_regression_coefs_nodelay.parquet")
-# write_parquet(coefs_df, out_filename)
+ 
+ 
+# ---- Oster (2019) robustness check
+
+vars1 <- c(atypicality)
+vars2 <- c(project_type, physical, letters, hearing, sfx_fe, cd_fe, yr_fe, cluster_fe)
+
+short_reg <- lm(
+  build_fmla("outcome_y", vars1),
+  data = df
+)
+controlled_reg <- lm(
+  build_fmla("outcome_y", c(vars1, vars2)),
+  data = df
+)
+Rmax <- min(1.3*summary(controlled_reg)$r.squared, 1)
+
+
+oster_delta <- o_delta(
+  y = "outcome_y",
+  x = "atypicality",
+  con = paste0(vars2, collapse=" + "),
+  beta = 0,
+  R2max = Rmax,
+  type = "lm",
+  data = df
+)
+
+out_filename <- paste0(DATA_PATH, "/intermediate_data/cpc/ologit_oster_delta.parquet")
+write_parquet(oster_delta, out_filename)
+
+
+# ---- Marginals
+
+m4 <- avg_slopes(r4)
+m4$regression_name <- "r4"
+
+marginals_df <- m4
+ 
+out_filename <- paste0(DATA_PATH, "/intermediate_data/cpc/ologit_regression_marginals.parquet")
+write_parquet(marginals_df, out_filename)
+
+
+
+
+# ---- Regressions on non-delay outcomes
+
+dfb <- filter(df, project_result!="DELAYED")
+
+dfb$outcome <- dfb$outcome_y
+dfb$outcome[dfb$outcome==1] <- 0
+dfb$outcome[dfb$outcome==2] <- 1
+
+rnullb <- glm(outcome ~ 1, data=dfb, family=binomial(link="logit"))
+nullb_LL <- as.numeric(logLik(rnullb))
+
+r1b <- glm(
+  build_fmla("outcome", c(project_type, physical, letters, hearing, atypicality, sfx_fe)),
+  data=dfb, family=binomial(link="logit")
+)
+r2b <- glm(
+  build_fmla("outcome", c(project_type, physical, letters, hearing, atypicality, sfx_fe, cd_fe)),
+  data=dfb, family=binomial(link="logit")
+)
+r3b <- glm(
+  build_fmla("outcome", c(project_type, physical, letters, hearing, atypicality, sfx_fe, cd_fe, yr_fe)),
+  data=dfb, family=binomial(link="logit")
+)
+r4b <- glm(
+  build_fmla("outcome", c(project_type, physical, letters, hearing, atypicality, sfx_fe, cd_fe, yr_fe, cluster_fe)),
+  data=dfb, family=binomial(link="logit")
+)
+
+stargazer(
+  r1b, r2b, r3b, r4b, type="text",
+  keep=keepvars,
+  add.lines=list(
+    c("Suffix Group Dummies",      "Y", "Y", "Y", "Y"),
+    c("Council District Dummies",  "N", "Y", "Y", "Y"),
+    c("Year Dummies",              "N", "N", "Y", "Y"),
+    c("Embedding Cluster Dummies", "N", "N", "N", "Y")
+  )
+)
+
+
+coefs_df <- rbind(
+  extract_reg(r1b, "r1b", nullb_LL),
+  extract_reg(r2b, "r2b", nullb_LL),
+  extract_reg(r3b, "r3b", nullb_LL),
+  extract_reg(r4b, "r4b", nullb_LL)
+)
+
+out_filename <- paste0(DATA_PATH, "/intermediate_data/cpc/ologit_regression_coefs_nodelay.parquet")
+write_parquet(coefs_df, out_filename)
